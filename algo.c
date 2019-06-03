de974bae2ad546dc8a41af12543727f65e0f2296
Julian Wisozk <jwisozk@at-l8.21-school.ru>
2019-06-03 22:01:10 +0300

    Done bresenham algorithm which works in all directions

--- algo.c
+++ algo.c
@@ -8,9 +8,16 @@ void ft_bresenham_hor(int x1, int y1, int x2, int y2, void *mlx_ptr, void *win_p
     int x;
     int y;
     int slope;
+    int yi;
 
     dx = x2 - x1;
     dy = y2 - y1;
+    yi = 1;
+    if (dy < 0)
+    {
+        yi = -1;
+        dy = -dy;
+    }
     slope = 2 * dy - dx;
     x = x1;
     y = y1;
@@ -19,7 +26,7 @@ void ft_bresenham_hor(int x1, int y1, int x2, int y2, void *mlx_ptr, void *win_p
         mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
         if (slope >= 0)
         {
-            y++;
+            y = y + yi;
             slope -= 2 * dx;
         }
         slope += 2 * dy;
@@ -34,9 +41,16 @@ void ft_bresenham_ver(int x1, int y1, int x2, int y2, void *mlx_ptr, void *win_p
     int x;
     int y;
     int slope;
+    int xi;
     
     dx = x2 - x1;
     dy = y2 - y1;
+    xi = 1;
+    if (dx < 0)
+    {
+        xi = -1;
+        dx = -dx;
+    }
     slope = 2 * dx - dy;
     x = x1;
     y = y1;
@@ -45,7 +59,7 @@ void ft_bresenham_ver(int x1, int y1, int x2, int y2, void *mlx_ptr, void *win_p
         mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
         if (slope >= 0)
         {
-            x++;
+            x = x + xi;
             slope -= 2 * dy;
         }
         slope += 2 * dx;
@@ -53,11 +67,29 @@ void ft_bresenham_ver(int x1, int y1, int x2, int y2, void *mlx_ptr, void *win_p
     }
 }
 
+void ft_bresenham(int x1, int y1, int x2, int y2, void *mlx_ptr, void *win_ptr, int color)
+{
+    if (abs(y2 - y1) < abs(x2 - x1))
+    {
+        if (x1 < x2)
+            ft_bresenham_hor(x1, y1, x2, y2, mlx_ptr, win_ptr, color);
+        else
+            ft_bresenham_hor(x2, y2, x1, y1, mlx_ptr, win_ptr, color);
+    }
+    else
+    {
+        if (y1 < y2)
+            ft_bresenham_ver(x1, y1, x2, y2, mlx_ptr, win_ptr, color);
+        else
+            ft_bresenham_ver(x2, y2, x1, y1, mlx_ptr, win_ptr, color);
+    }
+}
+
 void    ft_draw_lines(t_point **arr_lst, int len_x, int len_y, void *mlx_ptr, void *win_ptr)
 {
     int     i;
     int     j;
-//    printf("dx : %f, dy : %f\n", dx , dy);
+
     i = 0;
     while (i < len_y)
     {
@@ -65,16 +97,15 @@ void    ft_draw_lines(t_point **arr_lst, int len_x, int len_y, void *mlx_ptr, vo
         while (j < len_x)
         {
             if (j + 1 < len_x)
-                ft_bresenham_hor(arr_lst[i][j].x, arr_lst[i][j].y, arr_lst[i][j + 1].x, arr_lst[i][j + 1].y, mlx_ptr, win_ptr, 0xFFFFFF);
+                ft_bresenham(arr_lst[i][j].x, arr_lst[i][j].y, arr_lst[i][j + 1].x, arr_lst[i][j + 1].y, mlx_ptr, win_ptr, 0xFFFFFF);
             if (i + 1 < len_y)
-                ft_bresenham_ver(arr_lst[i][j].x, arr_lst[i][j].y, arr_lst[i + 1][j].x, arr_lst[i + 1][j].y, mlx_ptr, win_ptr, 0xFFFFFF);
-//          printf("(%i, %i) ", (int)arr_lst[i][j].x, (int)arr_lst[i][j].y);
+                ft_bresenham(arr_lst[i][j].x, arr_lst[i][j].y, arr_lst[i + 1][j].x, arr_lst[i + 1][j].y, mlx_ptr, win_ptr, 0xFFFFFF);
             j++;
         }
-//        printf("\n");
         i++;
     }
 }
+
 
 void    ft_open_window(t_point **arr_lst, int len_x, int len_y)
 {
@@ -87,9 +118,8 @@ void    ft_open_window(t_point **arr_lst, int len_x, int len_y)
 //    len_y = 1;
 //    arr_lst[0][0].x = 11;
     ft_draw_lines(arr_lst, len_x, len_y, mlx_ptr, win_ptr);
-//    ft_bresenham(100, 100, 100, 136, mlx_ptr, win_ptr, 0xFFFFFF);
+//    ft_bresenham(100, 100, 300, 300, mlx_ptr, win_ptr, 0xFFFFFF);
 //    ft_iso_modifier(mlx_ptr, win_ptr, arr_lst, len_x, len_y);
-
     mlx_loop(mlx_ptr);
 }
 
